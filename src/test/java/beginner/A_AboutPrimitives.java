88752e63a01587ac1c56378f601aa8ebaeb5bdff
unknown <lesiakowski@plkrnb026.transporeon.nil>
2014-05-07 16:32:07 +0200

    problems resolved

--- src/test/java/beginner/A_AboutPrimitives.java
+++ src/test/java/beginner/A_AboutPrimitives.java
@@ -2,7 +2,7 @@ package beginner;
 
 import org.junit.Test;
 
-import static constant.Constants.__;
+//import static constant.Constants.__;
 import static org.junit.Assert.assertEquals;
 
 public class A_AboutPrimitives {
@@ -11,23 +11,23 @@ public class A_AboutPrimitives {
 	public void integersHaveAFairlyLargeRange() {
         int a = 2147483647;
         int b = 2;
-		assertEquals(Integer.MIN_VALUE, -1);
-		assertEquals(Integer.MAX_VALUE, __);
-        assertEquals(Integer.SIZE, __);
-        assertEquals(a+b, __);
+		assertEquals(Integer.MIN_VALUE, -2147483648);
+		assertEquals(Integer.MAX_VALUE, a);
+        assertEquals(Integer.SIZE, 32);
+        assertEquals(a+b, -2147483647);
 	}
 
 	@Test
 	public void longsHaveALargerRangeThanInts() {
-        assertEquals(Long.MIN_VALUE, __);
-		assertEquals(Long.MAX_VALUE, __);
-        assertEquals(Long.SIZE, __);
+        assertEquals(Long.MIN_VALUE, -9223372036854775808L);
+		assertEquals(Long.MAX_VALUE, 9223372036854775807L);
+        assertEquals(Long.SIZE, 64);
 	}
 
 	@Test
 	public void doublesHaveALargeRange() {
-		assertEquals(Double.MIN_VALUE, __);
-		assertEquals(Double.MAX_VALUE, __);
-        assertEquals(Double.SIZE, __);
+		assertEquals(Double.MIN_VALUE, 4.9E-324,0.0001);
+		assertEquals(Double.MAX_VALUE, 1.7976931348623157E308,0.0001);
+        assertEquals(Double.SIZE, 64);
 	}
 }
