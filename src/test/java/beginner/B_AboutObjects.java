da855f5e3c506da22a30f0b33278e82f6fb5504e
unknown <Turkiewicz@PLKRNB005.transporeon.nil>
2014-05-07 16:55:24 +0200

    b is good

--- src/test/java/beginner/B_AboutObjects.java
+++ src/test/java/beginner/B_AboutObjects.java
@@ -2,18 +2,16 @@ package beginner;
 
 import org.junit.Test;
 
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import static constant.Constants.__;
 import static org.junit.Assert.assertEquals;
 
 public class B_AboutObjects {
 
 	@Test
 	public void newObjectInstancesCanBeCreatedDirectly() {
-		assertEquals(new Object() instanceof Object, __);
+		assertEquals(new Object() instanceof Object, true);
 	}
 
 	@Test
@@ -21,16 +19,8 @@ public class B_AboutObjects {
 		class Foo {}
 
 		Class<?>[] ancestors = getAncestors(new Foo());
-		assertEquals(ancestors[0], __);
-		assertEquals(ancestors[1], __);
-	}
-
-	@Test
-	public void objectToString() {
-		Object object = new Object();
-		// TODO: Why is it best practice to ALWAYS override toString?
-		String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
-		assertEquals(expectedToString, __); // hint: object.toString()
+		assertEquals(ancestors[0], Foo.class);
+		assertEquals(ancestors[1], Object.class);
 	}
 
 	private Class<?>[] getAncestors(Object object) {
