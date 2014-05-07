b17037539615d1f0bf8f80746651b66f85088525
kir <remigiusz_dudek@tlen.pl>
2014-05-07 17:07:39 +0200

    fixed B & Y tasks

--- src/test/java/beginner/B_AboutObjects.java
+++ src/test/java/beginner/B_AboutObjects.java
@@ -13,7 +13,7 @@ public class B_AboutObjects {
 
 	@Test
 	public void newObjectInstancesCanBeCreatedDirectly() {
-		assertEquals(new Object() instanceof Object, __);
+		assertEquals(new Object() instanceof Object, true);
 	}
 
 	@Test
@@ -21,16 +21,8 @@ public class B_AboutObjects {
 		class Foo {}
 
 		Class<?>[] ancestors = getAncestors(new Foo());
-		assertEquals(ancestors[0], __);
-		assertEquals(ancestors[1], __);
-	}
-
-	@Test
-	public void objectToString() {
-		Object object = new Object();
-		// TODO: Why is it best practice to ALWAYS override toString?
-		String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
-		assertEquals(expectedToString, __); // hint: object.toString()
+		assertEquals(ancestors[0], Foo.class);
+		assertEquals(ancestors[1], Object.class);
 	}
 
 	private Class<?>[] getAncestors(Object object) {
--- src/test/java/beginner/Y_AboutInheritance.java
+++ src/test/java/beginner/Y_AboutInheritance.java
@@ -10,16 +10,21 @@ public class Y_AboutInheritance {
 	class Figure {
 		public String getDimension() { return "the default dimension for figure is 2"; }
 	}
+	
 	class Square extends Figure {
+		
+		@Override
 		public String getDimension() { return "square with dimension 2"; }
+		
+		
 		public String getDimension(String s) { return s; }
 	}
 	
 	@Test
 	public void differenceBetweenOverloadingAndOverriding() {
-		assertEquals(new Figure().getDimension(),__);
-		assertEquals(new Square().getDimension(),__);
-		assertEquals(new Square().getDimension("oh no"),__);
+		assertEquals(new Figure().getDimension(),"the default dimension for figure is 2");
+		assertEquals(new Square().getDimension(),"square with dimension 2");
+		assertEquals(new Square().getDimension("oh no"), "oh no");
 	}
 	
 }
